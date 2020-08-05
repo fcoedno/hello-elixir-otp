49358a538bf29485d5327e0379fef70755d205b3
Francisco Edno <ednofco@gmail.com>
2020-08-04 22:06:20 -0300

    Implement a Bucket with Agents

---
+++ test/kv/bucket_test.exs
@@ -0,0 +1,25 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  alias KV.Bucket
+
+  setup do
+    {:ok, bucket} = Bucket.start_link([])
+    %{bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
+    Bucket.put(bucket, "milk", 3)
+
+    assert Bucket.get(bucket, "milk") == 3
+    assert Bucket.get(bucket, "bread") == nil
+  end
+
+  test "Deletes a keys", %{bucket: bucket} do
+    Bucket.put(bucket, "milk", 3)
+    deleted_value = Bucket.delete(bucket, "milk")
+
+    assert Bucket.get(bucket, "milk") == nil
+    assert deleted_value == 3
+  end
+end
